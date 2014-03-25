1bb68ddc5b2c92f0f201b3ad1352d80d613ce7b0
Josh Lawrence <jlawrence@webershandwick.com>
2014-03-25 12:25:13 -0400

    cleaned up styles and scripts

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -1,4 +1,25 @@
 /**
+ * Add Google Analytics tracking code
+ */
+(function(i, s, o, g, r, a, m) {
+	i['GoogleAnalyticsObject'] = r;
+	i[r] = i[r] || function() {
+		(i[r].q = i[r].q || []).push(arguments)
+	}, i[r].l = 1 * new Date();
+	a = s.createElement(o),
+	m = s.getElementsByTagName(o)[0];
+	a.async = 1;
+	a.src = g;
+	m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+/**
+ * Set up GA account and log pageview
+ */
+ga('create', 'UA-XXXX-Y');
+ga('send', 'pageview');
+
+/**
  * Logs an event to Google Analytics using the element's data attributes.
  * Adapted from Ben Plum's Scout plugin: https://github.com/benplum/Scout
  * @param  {object} e The event that fired the function
@@ -34,25 +55,14 @@ var trackEvent = function (e) {
 	}
 };
 
-////////////////////
-// Event Handlers //
-////////////////////
-
+/**
+ * Event Handlers
+ */
 $(document).on('click', '[data-track-event]', trackEvent);
 
-////////////////
-// Initialize //
-////////////////
-
-var init = function () {
-	// code to run on DOM ready...
-};
-
-////////////////
-// Public API //
-////////////////
-
+/**
+ * Public API
+ */
 module.exports = {
-	init: init,
 	trackEvent: trackEvent
-}
+};
