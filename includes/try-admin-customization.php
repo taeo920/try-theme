53db60d6cdcf3b3581e78a3b5c2eb2a6f050ca75
Steve Morton <smorton@mindgrub.com>
2016-03-09 15:28:06 -0500

    updated remove dashboard widgets to use remove_meta_box rather than interact directly with the metabox array

--- includes/try-admin-customization.php
+++ includes/try-admin-customization.php
@@ -4,14 +4,14 @@
  * Remove dashboard meta boxes
  */
 function try_remove_dashboard_widgets() {
-	global $wp_meta_boxes;
-	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
-	// unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_activity']);
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
-	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
-	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
+    // remove_meta_box('dashboard_right_now', 'dashboard', 'normal'); // Right Now
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal'); // Recent Comments
+    remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal'); // Incoming Links
+    remove_meta_box('dashboard_plugins', 'dashboard', 'normal'); // Plugins
+    remove_meta_box('dashboard_quick_press', 'dashboard', 'side'); // Quick Press
+    remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side'); // Recent Drafts
+    remove_meta_box('dashboard_primary', 'dashboard', 'side'); // WordPress blog
+    remove_meta_box('dashboard_secondary', 'dashboard', 'side'); // Other WordPress News
 }
 add_action('wp_dashboard_setup', 'try_remove_dashboard_widgets' );
 
